84a3fd57a72b5f7a9ab75f3e245feb12dfc9281d
zurble <elbruzmurat@gmail.com>
2024-11-06 23:17:56 +0300

    Groups work -updatable drawable-

--- main.py
+++ main.py
@@ -10,7 +10,14 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    # Create empty groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
+
     dt = 0
 
     while True:
@@ -20,8 +27,13 @@ def main():
                 return
 
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+
+        # initiate groups
+        for entity in updatable:
+            entity.update(dt)
+        for entity in drawable:
+            entity.draw(screen)
+
         # update content on the display screen
         pygame.display.flip()
         # limit the framerate to 60 FPS
